ef8878ff7fe1d001a1cba2cd09cc190253fd34b8
chogunny1 <gunnychocho@gmail.com>
2022-07-12 18:19:00 +0900

    ㅇ
    ㅇㅇ

---
+++ Assets/08.Script/Audio/AudioPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public enum MixerChannel
+{
+    Master,Tile,Music,Effect
+}
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+    public AudioClip clip;
+    public MixerChannel channel;
+    public AudioMixer mixer;
+
+    [ContextMenu("Play")]
+    public void Play()
+    {
+        audioSource.outputAudioMixerGroup = mixer.FindMatchingGroups(channel.ToString())[0];
+        audioSource.Stop();
+        if(clip != null)
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+}
